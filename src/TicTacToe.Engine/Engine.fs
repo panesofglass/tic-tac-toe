7fc5ec556c8d77ad8c8d58619a5596c7fc58c4f3
Ryan Riley <ryanriley@live.com>
2025-04-14 19:00:26 -0500

    feat: implement data structures and functions to play game

--- src/TicTacToe.Engine/Engine.fs
+++ src/TicTacToe.Engine/Engine.fs
@@ -1,24 +1,23 @@
 ﻿module TicTacToe.Engine
 
-[<StructuralEquality; StructuralComparison>]
-[<Struct>]
-type ColumnPosition =
-    | Left
-    | Center
-    | Right
+open System.Collections.Generic
 
 [<StructuralEquality; StructuralComparison>]
 [<Struct>]
-type RowPosition =
-    | Top
-    | Middle
-    | Bottom
-
-type SquarePosition = (struct(ColumnPosition * RowPosition))
+type SquarePosition =
+    | TopLeft
+    | TopCenter
+    | TopRight
+    | MiddleLeft
+    | MiddleCenter
+    | MiddleRight
+    | BottomLeft
+    | BottomCenter
+    | BottomRight
 
 [<StructuralEquality; StructuralComparison>]
 [<Struct>]
-type Player = PlayerX | PlayerO
+type Player = X | O
 
 [<StructuralEquality; StructuralComparison>]
 [<Struct>]
@@ -26,34 +25,141 @@ type SquareState =
     | Taken of Player
     | Empty
 
+[<StructuralEquality; StructuralComparison>]
 [<Struct>]
-type Square = { Position: SquarePosition; State: SquareState }
-
-[<Struct>]
-type GameState = { Squares: Square list }
-
-type StartGame<'GameState> = unit -> 'GameState
+type XPosition = XPos of SquarePosition
 
 [<StructuralEquality; StructuralComparison>]
 [<Struct>]
-type PlayerXPos = PlayerXPos of Square
+type OPosition = OPos of SquarePosition
 
-[<StructuralEquality; StructuralComparison>]
-[<Struct>]
-type PlayerOPos = PlayerOPos of Square
+type ValidMovesForX = XPosition[]
 
-type ValidMovesForPlayerX = PlayerXPos list
-
-type ValidMovesForPlayerO = PlayerOPos list
+type ValidMovesForO = OPosition[]
 
 type MoveResult<'GameState> =
-    | PlayerXTurn of 'GameState * ValidMovesForPlayerX
-    | PlayerOTurn of 'GameState * ValidMovesForPlayerO
+    | XTurn of 'GameState * ValidMovesForX
+    | OTurn of 'GameState * ValidMovesForO
     | Won of 'GameState * Player
     | Draw of 'GameState
 
-type PlayerXMove<'GameState> =
-    'GameState * PlayerXPos -> MoveResult<'GameState>
+type StartGame<'GameState> = unit -> MoveResult<'GameState>
 
-type PlayerOMove<'GameState> =
-    'GameState * PlayerOPos -> MoveResult<'GameState>
+[<StructuralEquality; StructuralComparison>]
+[<Struct>]
+type Move =
+    | XMove of SquarePosition
+    | OMove of SquarePosition
+
+type XMove<'GameState> =
+    MoveResult<'GameState> * XPosition -> MoveResult<'GameState>
+
+type OMove<'GameState> =
+    MoveResult<'GameState> * OPosition -> MoveResult<'GameState>
+
+type MakeMove<'GameState> = MoveResult<'GameState> * Move -> MoveResult<'GameState>
+
+
+type GameState = IReadOnlyDictionary<SquarePosition, SquareState>
+
+
+let startGame: StartGame<GameState> = fun () ->
+    let gameState =
+        [| TopLeft, Empty
+           TopCenter, Empty
+           TopRight, Empty
+           MiddleLeft, Empty
+           MiddleCenter, Empty
+           MiddleRight, Empty
+           BottomLeft, Empty
+           BottomCenter, Empty
+           BottomRight, Empty |]
+        |> readOnlyDict
+    let validMovesForX: ValidMovesForX =
+        [| for KeyValue(pos, state) in gameState do
+            if state = Empty then yield XPos pos |]
+    XTurn (gameState, validMovesForX)
+
+let winningCombinations =
+    [| [| TopLeft; TopCenter; TopRight |]
+       [| MiddleLeft; MiddleCenter; MiddleRight |]
+       [| BottomLeft; BottomCenter; BottomRight |]
+       [| TopLeft; MiddleLeft; BottomLeft |]
+       [| TopCenter; MiddleCenter; BottomCenter |]
+       [| TopRight; MiddleRight; BottomRight |]
+       [| TopLeft; MiddleCenter; BottomRight |]
+       [| TopRight; MiddleCenter; BottomLeft |] |]
+
+let (|HasWinner|_|) (gameState: GameState) =
+    let getWinningPlayer (combination: SquarePosition[]) =
+        let allSquares =
+            combination
+            |> Array.choose (fun pos -> 
+                match gameState.TryGetValue(pos) with
+                | true, Taken player -> Some player
+                | _ -> None)
+
+        if allSquares.Length = combination.Length && 
+           Array.forall (fun p -> p = allSquares[0]) allSquares then
+            Some allSquares[0]
+        else
+            None
+
+    winningCombinations
+    |> Array.tryPick getWinningPlayer
+
+let (|IsDraw|_|) (gameState: GameState) =
+    if gameState.Values |> Seq.forall (fun state -> state <> Empty) then
+        Some()
+    else
+        None
+
+let moveX: XMove<GameState> = fun (moveResult, XPos xPosition) ->
+    match moveResult with
+    | XTurn (gameState, _) ->
+        match gameState.TryGetValue(xPosition) with
+        | true, Empty ->
+            let gameState' =
+                [| for KeyValue(pos, state) in gameState ->
+                    pos, if pos = xPosition then Taken X else state |]
+                |> readOnlyDict
+
+            match gameState' with
+            | HasWinner player -> Won(gameState', player)
+            | IsDraw -> Draw(gameState')
+            | _ ->
+                let validMovesForO: ValidMovesForO =
+                    [| for KeyValue(pos, state) in gameState do
+                        if state = Empty then yield OPos pos |]
+                OTurn(gameState', validMovesForO)
+        | _ -> moveResult
+    | _ -> moveResult
+
+let moveO: OMove<GameState> = fun (moveResult, OPos oPosition) ->
+    match moveResult with
+    | XTurn (gameState, _) ->
+        match gameState.TryGetValue(oPosition) with
+        | true, Empty ->
+            let gameState' =
+                [| for KeyValue(pos, state) in gameState ->
+                    pos, if pos = oPosition then Taken O else state |]
+                |> readOnlyDict
+
+            match gameState' with
+            | HasWinner player -> Won(gameState', player)
+            | IsDraw -> Draw(gameState')
+            | _ ->
+                let validMovesForX: ValidMovesForX =
+                    [| for KeyValue(pos, state) in gameState do
+                        if state = Empty then yield XPos pos |]
+                XTurn(gameState', validMovesForX)
+        | _ -> moveResult
+    | _ -> moveResult
+
+let move (moveResult, move) =
+    match moveResult, move with
+    | XTurn _, XMove pos ->
+        moveX (moveResult, XPos pos)
+    | OTurn _, OMove pos ->
+        moveO (moveResult, OPos pos)
+    | moveResult, _ -> moveResult
---
+++ src/TicTacToe.Engine/Engine.fsi
@@ -0,0 +1,64 @@
+module TicTacToe.Engine
+
+open System.Collections.Generic
+
+[<StructuralEquality; StructuralComparison>]
+[<Struct>]
+type SquarePosition =
+    | TopLeft
+    | TopCenter
+    | TopRight
+    | MiddleLeft
+    | MiddleCenter
+    | MiddleRight
+    | BottomLeft
+    | BottomCenter
+    | BottomRight
+
+[<StructuralEquality; StructuralComparison>]
+[<Struct>]
+type Player = X | O
+
+[<StructuralEquality; StructuralComparison>]
+[<Struct>]
+type SquareState =
+    | Taken of Player
+    | Empty
+
+[<StructuralEquality; StructuralComparison>]
+[<Struct>]
+type XPosition = XPos of SquarePosition
+
+[<StructuralEquality; StructuralComparison>]
+[<Struct>]
+type OPosition = OPos of SquarePosition
+
+type ValidMovesForX = XPosition[]
+
+type ValidMovesForO = OPosition[]
+
+type MoveResult<'GameState> =
+    | XTurn of 'GameState * ValidMovesForX
+    | OTurn of 'GameState * ValidMovesForO
+    | Won of 'GameState * Player
+    | Draw of 'GameState
+
+type StartGame<'GameState> = unit -> MoveResult<'GameState>
+
+[<StructuralEquality; StructuralComparison>]
+[<Struct>]
+type Move =
+    | XMove of SquarePosition
+    | OMove of SquarePosition
+
+type XMove<'GameState> =
+    MoveResult<'GameState> * XPosition -> MoveResult<'GameState>
+
+type OMove<'GameState> =
+    MoveResult<'GameState> * OPosition -> MoveResult<'GameState>
+
+type MakeMove<'GameState> = MoveResult<'GameState> * Move -> MoveResult<'GameState>
+
+val startGame: StartGame<'GameState>
+
+val makeMove : MakeMove<'GameState>
